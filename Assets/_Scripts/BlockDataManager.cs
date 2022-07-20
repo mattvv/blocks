5af7876eae973177395d438712fc37ba1c0382b6
Matt Van <matt@opticpower.com>
2022-07-19 20:58:22 -0400

    Expose BlockData to unity

---
+++ Assets/_Scripts/BlockDataManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockDataManager : MonoBehaviour
+{
+    public static float textureOffset = 0.001f;
+    public static float tileSizeX, tileSizeY;
+
+    public static Dictionary<BlockType, TextureData> blockTextureDataDictionary =
+        new Dictionary<BlockType, TextureData>();
+
+    public BlockDataSO textureData;
+
+    public void Awake()
+    {
+        foreach (var item in textureData.textureDataList)
+        {
+            if (blockTextureDataDictionary.ContainsKey(item.blockType))
+            {
+                blockTextureDataDictionary.Add(item.blockType, item);
+            }
+        }
+
+        tileSizeX = textureData.textureSizeX;
+        tileSizeY = textureData.textureSizeY;
+    }
+}
